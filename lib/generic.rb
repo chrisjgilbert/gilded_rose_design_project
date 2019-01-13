d4b416edd5880d8b1355cda31cf5cda84d2484e3
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 12:37:41 +0000

    refactored generic item class out further into helper methods to set quality and check before/after sell_in

--- lib/generic.rb
+++ lib/generic.rb
@@ -1,11 +1,15 @@
+require_relative './item'
+
 class Generic < Item
 
 # name, sell_in, quality
   def update_quality
-    if after_sell_in?
-      @quality <= 1 ? @quality = 0 : @quality -= 2
+    if after_sell_in? and above_min_quality?
+      update_quality_by_2
+    elsif before_sell_in? and above_min_quality?
+      update_quality_by_1
     else
-      @quality <= 1 ? @quality = 0 : @quality -= 1
+      set_quality_at_0
     end
   end
 
@@ -13,6 +17,26 @@ class Generic < Item
 
   def after_sell_in?
     @sell_in <= 0
+  end
+
+  def above_min_quality?
+    @quality > 0
+  end
+
+  def before_sell_in?
+    @sell_in > 0
+  end
+
+  def update_quality_by_2
+    @quality -= 2
+  end
+
+  def update_quality_by_1
+    @quality -= 1
+  end
+
+  def set_quality_at_0
+    @quality = 0
   end
 
 end
