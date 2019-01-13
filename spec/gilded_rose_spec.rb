f9b144be7158a9bb70f809fa4cc262d418b0ecf8
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 13:04:47 +0000

    green: update quality does not change the name

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -6,22 +6,23 @@ describe GildedRose do
   describe "#update_quality" do
 
     it "does not change the name" do
-      item = Item.new("item", 1, 0)
-      GildedRose.new([item]).update_quality
-      expect(item.name).to eq "item"
+      aged_brie = double(:aged_brie, name: "aged brie")
+      allow(aged_brie).to receive(:update_quality)
+      GildedRose.new([aged_brie]).update_quality
+      expect(aged_brie.name).to eq "aged brie"
     end
 
-    it "lowers the sell_in by one after a day" do
+    xit "lowers the sell_in by one after a day" do
       item = Item.new("item", 1, 0)
       GildedRose.new([item]).update_quality
       expect(item.sell_in).to eq 0
     end
 
-    it "never lowers quality below 0" do
+    xit "never lowers quality below 0" do
       item = Item.new("item", 0, 0)
       GildedRose.new([item]).update_quality
       expect(item.quality).to eq(0)
     end
-
   end
+
 end
