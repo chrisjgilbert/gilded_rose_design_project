383605a0bbfd0f2eb35dcad868794f770c294264
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 11:14:57 +0000

    refactored

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -1,13 +1,23 @@
 class BackstagePass < Item
 
   def update_quality
-    if @quality < 50
-       if @sell_in <= 10 and @sell_in > 5
+    if below_max_quality?
+       if sell_in_less_than_10_and_more_than_5?
          @quality += 2
        else
          @quality += 1
        end
     end
+  end
+
+  private
+
+  def below_max_quality?
+    @quality < 50
+  end
+
+  def sell_in_less_than_10_and_more_than_5?
+    @sell_in <= 10 and @sell_in > 5
   end
 
 end
