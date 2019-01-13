793fbf2e9365869d2c17d2342ff0196f629a8b00
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 10:45:07 +0000

    aged brie update quality refactored into private helper methods

--- lib/aged_brie.rb
+++ lib/aged_brie.rb
@@ -4,9 +4,29 @@ require 'item'
 class AgedBrie < Item
 
   def update_quality
-    if @quality < 50
-      @sell_in > 0 ? @quality += 1 : @quality == 49 ? @quality += 1 : @quality += 2
+    if below_max_quality? and before_sell_in?
+      update_before_sell_in_quality
+    elsif below_max_quality? and not before_sell_in?
+      update_after_sell_in_quality
     end
+  end
+
+  private
+
+  def before_sell_in?
+    @sell_in > 0
+  end
+
+  def below_max_quality?
+    @quality < 50
+  end
+
+  def update_before_sell_in_quality
+    @quality += 1
+  end
+
+  def update_after_sell_in_quality
+    @quality == 49 ? @quality += 1 : @quality += 2
   end
 
 end
