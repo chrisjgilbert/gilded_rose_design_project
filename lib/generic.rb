6b40a2f0f7e3cbbdf85b724fd225aa91d2b81c4b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 12:31:10 +0000

    green: edge case tests added to prevent quality from going below 0

--- lib/generic.rb
+++ lib/generic.rb
@@ -1,11 +1,18 @@
 class Generic < Item
 
+# name, sell_in, quality
   def update_quality
-    if @sell_in <= 0
-      @quality -= 2
+    if after_sell_in?
+      @quality <= 1 ? @quality = 0 : @quality -= 2
     else
-      @quality -= 1
+      @quality <= 1 ? @quality = 0 : @quality -= 1
     end
+  end
+
+  private
+
+  def after_sell_in?
+    @sell_in <= 0
   end
 
 end
--- spec/generic_spec.rb
+++ spec/generic_spec.rb
@@ -11,11 +11,23 @@ describe Generic do
           generic.update_quality
           expect(generic.quality).to eq(0)
         end
+
+        it 'quality cant go lower than 0' do
+          generic = Generic.new("item", 4, 0)
+          generic.update_quality
+          expect(generic.quality).to eq(0)
+        end
       end
 
       context 'after sell_in' do
         it 'lowers quality by two after a day' do
           generic = Generic.new("item", 0, 2)
+          generic.update_quality
+          expect(generic.quality).to eq(0)
+        end
+
+        it 'quality cant go lower than 0' do
+          generic = Generic.new("item", 0, 0)
           generic.update_quality
           expect(generic.quality).to eq(0)
         end
