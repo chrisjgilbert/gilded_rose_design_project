ef9b7a0901a832bb2ddec6284c14d0b6f1876b2d
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 11:16:58 +0000

    green: only raises quality to 50 when quality is at 49

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -3,7 +3,7 @@ class BackstagePass < Item
   def update_quality
     if below_max_quality?
        if sell_in_less_than_10_and_more_than_5?
-         @quality += 2
+         update_middle_sell_in_period_quality
        else
          @quality += 1
        end
@@ -18,6 +18,10 @@ class BackstagePass < Item
 
   def sell_in_less_than_10_and_more_than_5?
     @sell_in <= 10 and @sell_in > 5
+  end
+
+  def update_middle_sell_in_period_quality
+    @quality == 49 ? @quality += 1 : @quality += 2
   end
 
 end
