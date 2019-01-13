e38efaf2b319088da1388175ea94204a3185f352
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 13:25:08 +0000

    red: generic sell_in lowers by one before sell_in when update called

--- spec/generic_spec.rb
+++ spec/generic_spec.rb
@@ -4,12 +4,6 @@ describe Generic do
 
   describe '#update_quality' do
 
-    it "does not change the name" do
-      item = Generic.new("foo", 0, 0)
-      item.update_quality
-      expect(item.name).to eq "foo"
-    end
-
     context 'before sell_in' do
       it 'lowers quality by one after one day' do
         generic = Generic.new("item", 1, 1)
@@ -21,6 +15,12 @@ describe Generic do
         generic = Generic.new("item", 4, 0)
         generic.update_quality
         expect(generic.quality).to eq(0)
+      end
+
+      it "lowers the sell_in by one after a day" do
+        generic = Generic.new("item", 1, 1)
+        generic.update_quality
+        expect(generic.sell_in).to eq 0
       end
     end
 
