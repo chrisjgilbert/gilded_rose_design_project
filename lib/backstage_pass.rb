308145a65f83c5ff87ad3bdb13583affd2a86aec
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 17:01:04 +0000

    extracted further quality limits into module

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -26,11 +26,11 @@ class BackstagePass < Generic
   end
 
   def update_quality_by_middle_of_sell_in_period_amount
-    @quality == 49 ? increase_quality_by_1 : increase_quality_by_2
+    quality_at_49? ? increase_quality_by_1 : increase_quality_by_2
   end
 
   def update_quality_by_end_of_sell_in_period_amount
-    @quality == 48 ? increase_quality_by_2 : increase_quality_by_3
+    quality_at_48? ? increase_quality_by_2 : increase_quality_by_3
   end
 
 end
--- lib/quality_limits.rb
+++ lib/quality_limits.rb
@@ -11,9 +11,17 @@ module QualityLimits
   def above_min_quality?
     @quality > 0
   end
-  
+
   def below_max_quality?
     @quality < 50
+  end
+
+  def quality_at_49?
+    @quality == 49
+  end
+
+  def quality_at_48?
+    @quality == 48
   end
 
 end
