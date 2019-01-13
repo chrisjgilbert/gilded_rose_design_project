9f40c245c6729309bba21bbc825162f2e125663b
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 11:44:31 +0000

    refactored backstagepass class out further into more helper methods and generally made labellign clearer

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -2,44 +2,58 @@ class BackstagePass < Item
 
 #name, sell_in, quality
   def update_quality
-    if below_max_quality? and during_start_of_sell_in_period?
-      update_start_sell_in_period
-    elsif below_max_quality? and during_middle_sell_in_period?
-      update_middle_sell_in_period_quality
-    elsif below_max_quality? and during_end_sell_in_period?
-      update_end_sell_in_period
-    elsif @sell_in <= 0
+    if during_sell_in_period? and below_max_quality?
+      update_based_on_sell_in_period
+    elsif after_sell_in_period?
       @quality = 0
+    end
+  end
+
+  def update_based_on_sell_in_period
+    if during_start_of_sell_in_period?
+      update_quality_by_start_of_sell_in_period_amount
+    elsif during_middle_of_sell_in_period?
+      update_quality_by_middle_of_sell_in_period_amount
+    elsif during_end_of_sell_in_period?
+      update_quality_by_end_of_sell_in_period_amount
     end
   end
 
   private
 
-  def below_max_quality?
-    @quality < 50
+  def during_sell_in_period?
+    @sell_in > 0
   end
 
-  def update_start_sell_in_period
-    @quality += 1
+  def after_sell_in_period?
+    @sell_in <= 0
+  end
+
+  def below_max_quality?
+    @quality < 50
   end
 
   def during_start_of_sell_in_period?
     @sell_in > 10
   end
 
-  def during_middle_sell_in_period?
+  def update_quality_by_start_of_sell_in_period_amount
+    @quality += 1
+  end
+
+  def during_middle_of_sell_in_period?
     @sell_in <= 10 and @sell_in > 5
   end
 
-  def update_middle_sell_in_period_quality
+  def update_quality_by_middle_of_sell_in_period_amount
     @quality == 49 ? @quality += 1 : @quality += 2
   end
 
-  def during_end_sell_in_period?
+  def during_end_of_sell_in_period?
     @sell_in <= 5 and @sell_in> 0
   end
 
-  def update_end_sell_in_period
+  def update_quality_by_end_of_sell_in_period_amount
     @quality == 48 ? @quality += 2 : @quality += 3
   end
 
