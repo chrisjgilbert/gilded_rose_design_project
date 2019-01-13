44baa3f2fd77b95ca36ecfc6705b8e3d7232ac6e
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 15:26:36 +0000

    red: sulfuras update sell in does not change sell in

--- spec/sulfuras_spec.rb
+++ spec/sulfuras_spec.rb
@@ -3,7 +3,6 @@ require 'sulfuras'
 describe Sulfuras do
 
   describe "#update_quality" do
-
     context 'when item is Sulfuras, Hand of Ragnaros' do
       it 'does not change sell_in' do
         sulfuras = Sulfuras.new("Sulfuras, Hand of Ragnaros", 1, 80)
@@ -15,6 +14,14 @@ describe Sulfuras do
         sulfuras = Sulfuras.new("Sulfuras, Hand of Ragnaros", 1, 80)
         sulfuras.update_quality
         expect(sulfuras.quality).to eq(80)
+      end
+    end
+
+    describe "#update_sell_in" do
+      it 'does not change sell_in' do
+        sulfuras = Sulfuras.new("Sulfuras, Hand of Ragnaros", 1, 80)
+        sulfuras.update_sell_in
+        expect(sulfuras.sell_in).to eq(1)
       end
     end
 
