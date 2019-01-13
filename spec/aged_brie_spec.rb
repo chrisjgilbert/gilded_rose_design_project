668476bb821194e857e2824ebdee94becae09c08
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 09:56:53 +0000

    aged brie tests all red

--- spec/aged_brie_spec.rb
+++ spec/aged_brie_spec.rb
@@ -1,29 +1,35 @@
-context 'when item is Aged Brie' do
-  context 'before sell_in' do
-    it 'raises quality by one after a day' do
-      item = Item.new("Aged Brie", 1, 0)
-      GildedRose.new([item]).update_quality
-      expect(item.quality).to eq(1)
+require 'aged_brie'
+
+describe AgedBrie do
+
+  context 'when item is Aged Brie' do
+    context 'before sell_in' do
+      it 'raises quality by one after a day' do
+        item = Item.new("Aged Brie", 1, 0)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(1)
+      end
+
+      it 'never raises quality beyond 50' do
+        item = Item.new("Aged Brie", 1, 50)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(50)
+      end
     end
 
-    it 'never raises quality beyond 50' do
-      item = Item.new("Aged Brie", 1, 50)
-      GildedRose.new([item]).update_quality
-      expect(item.quality).to eq(50)
+    context 'after sell_in' do
+      it 'raises quality by two after a day' do
+        item = Item.new("Aged Brie", 0, 0)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(2)
+      end
+
+      it 'only raises quality to 50 when quality is at 49' do
+        item = Item.new("Aged Brie", 0, 49)
+        GildedRose.new([item]).update_quality
+        expect(item.quality).to eq(50)
+      end
     end
   end
 
-  context 'after sell_in' do
-    it 'raises quality by two after a day' do
-      item = Item.new("Aged Brie", 0, 0)
-      GildedRose.new([item]).update_quality
-      expect(item.quality).to eq(2)
-    end
-
-    it 'only raises quality to 50 when quality is at 49' do
-      item = Item.new("Aged Brie", 0, 49)
-      GildedRose.new([item]).update_quality
-      expect(item.quality).to eq(50)
-    end
-  end
 end
