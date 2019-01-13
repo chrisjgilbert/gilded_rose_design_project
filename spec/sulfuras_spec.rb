d980701056d045a4c98d707ff25371d4c5cb46c2
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 10:55:34 +0000

    sulfuras tests updated to reflect new class

--- spec/sulfuras_spec.rb
+++ spec/sulfuras_spec.rb
@@ -4,18 +4,18 @@ describe Sulfuras do
 
     context 'when item is Sulfuras, Hand of Ragnaros' do
       it 'does not change sell_in' do
-        item = Item.new("Sulfuras, Hand of Ragnaros", 1, 80)
-        GildedRose.new([item]).update_quality
-        expect(item.sell_in).to eq(1)
+        sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 1, 80)
+        sulfuras.update_quality
+        expect(sulfuras.sell_in).to eq(1)
       end
 
       it 'does not change quality' do
-        item = Item.new("Sulfuras, Hand of Ragnaros", 1, 80)
-        GildedRose.new([item]).update_quality
+        sulfuras = Item.new("Sulfuras, Hand of Ragnaros", 1, 80)
+        sulfuras.update_quality
         expect(item.quality).to eq(80)
       end
     end
 
   end
-  
+
 end
