e962919af6bce823d3d882f2e9088b75c7ddbd89
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 13:10:41 +0000

    test added to check that update quality gets called on double added as an item

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,6 +5,12 @@ describe GildedRose do
 
   describe "#update_quality" do
 
+    it "tells the item to update it's quality" do
+      aged_brie = double(:aged_brie, name: "aged brie")
+      expect(aged_brie).to receive(:update_quality)
+      GildedRose.new([aged_brie]).update_quality
+    end
+
     it "does not change the name" do
       aged_brie = double(:aged_brie, name: "aged brie")
       allow(aged_brie).to receive(:update_quality)
