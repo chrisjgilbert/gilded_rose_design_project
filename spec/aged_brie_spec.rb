0fd14b257f0e20ba315904f9a4e5c531882913bc
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 10:13:36 +0000

    red: aged brie raises quality by 2 after sell in date passes

--- spec/aged_brie_spec.rb
+++ spec/aged_brie_spec.rb
@@ -18,10 +18,10 @@ describe AgedBrie do
     end
 
     context 'after sell_in' do
-      xit 'raises quality by two after a day' do
-        item = Item.new("Aged Brie", 0, 0)
-        GildedRose.new([item]).update_quality
-        expect(item.quality).to eq(2)
+      it 'raises quality by two after a day' do
+        aged_brie = AgedBrie.new("Aged Brie", 0, 0)
+        aged_brie.update_quality
+        expect(aged_brie.quality).to eq(2)
       end
 
       xit 'only raises quality to 50 when quality is at 49' do
