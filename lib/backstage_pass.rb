f46a3abeeb968ff6a7359fcbc7911946601d6d83
Chris Gilbert <chrisgilbert4@icloud.com>
2019-01-13 11:12:33 +0000

    green: increase quality by 2 when sellin <= 10 && > 5

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -2,7 +2,11 @@ class BackstagePass < Item
 
   def update_quality
     if @quality < 50
-      @quality += 1
+       if @sell_in <= 10 and @sell_in > 5
+         @quality += 2
+       else
+         @quality += 1
+       end
     end
   end
 
